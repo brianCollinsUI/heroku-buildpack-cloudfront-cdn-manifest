4fbc0f754b11306e4ef281c4feb911b5651c4171
brianCollinsUI <brian@userinterviews.com>
2022-08-15 21:19:08 -0700

    initial commit

---
+++ lib/download_file.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+download_file () {
+  local remote_path=$1
+
+  curl "https://assets.userinterviews.com/${remote_path}" >> public/packs/mainfest.json # TODO need url to be based on ENV
+
+  local status=$?
+
+  if [[ status -eq 0 ]]; then
+    echo "Downloaded remote file to public/packs/mainfest.json" | indent
+  fi
+
+  return $status
+}

7613e1da764cfe1543116525436a5a1d60fb8ac9
brianCollinsUI <brian@userinterviews.com>
2022-08-19 08:47:42 -0700

    Update download_file.sh

--- lib/download_file.sh
+++ lib/download_file.sh
@@ -3,7 +3,7 @@
 download_file () {
   local remote_path=$1
 
-  curl "https://assets.userinterviews.com/${remote_path}" >> public/packs/mainfest.json # TODO need url to be based on ENV
+  curl "https://assets.userinterviews.com/${remote_path}" >> public/assets/.sprockets-mainfest-$SOURCE_VERSION.json
 
   local status=$?
 
